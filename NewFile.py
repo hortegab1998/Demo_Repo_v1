a820832aa4deaf33ce2028b0136d6b9210b118fe
hortegab1998 <ortegahenry1998@gmail.com>
2023-11-07 16:12:21 -0800

    hello

--- NewFile.py
+++
@@ -1,9 +0,0 @@
-#What is this:
-
-
-#Im making some changes to this file and will add the changes into  a new branch
-
-name =  input('Please Enter Name: ')
-print(f'Hello There {name}')
-
-print('Sike')
